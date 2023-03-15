c1a7c808b16a5f70ee4cbeaf6c1748a713393f58
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-03-15 14:12:16 +0100

    added nav

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './style/style.scss';
+
+const menuBtn = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('nav');
+
+function openMenu() {
+  menu?.classList.toggle('open-mobile');
+  menuBtn?.classList.toggle('open');
+}
+
+menuBtn?.addEventListener('click', openMenu);
